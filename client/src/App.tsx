fd938ef9536da17ee4a7f17cce30111910cbead9
toytoy08 <toy31327@gmail.com>
2023-03-25 05:35:25 +0700

    add backend api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -51,7 +51,7 @@ function App() {
       const profileObj = credential ? parseJwt(credential) : null;
       
       if (profileObj) {
-        const response = await fetch("http://localhost:8080/api/v1/users", {
+        const response = await fetch("https://refine-blog-app.onrender.com/api/v1/users", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
@@ -119,7 +119,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://refine-blog-app.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
